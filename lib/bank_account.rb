e4f9ca6aa1e621a8516a06bfd7236d840487b2e0
BMoClay <benmorgancleveland@gmail.com>
2020-10-30 13:12:12 -0400

    first commit w Amelia

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
+require_relative './transfer.rb'
+
 class BankAccount
 
+    attr_accessor :balance, :status, :transfer
+    attr_reader :name
+
+    def initialize(name, balance=1000)
+        @name = name
+        @balance = balance.to_i
+        @status = "open"
+    end
+    
+    def deposit(money)
+        @balance += money
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0 
+    end
+
+    def close_account
+        @status = "closed"
+    end
 end
